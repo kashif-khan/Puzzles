38929b4fe3a2028ae83c50bf7d2ba942e7f9b05d
kashif-khan <uplink2010@gmail.com>
2018-03-21 19:33:36 +0530

    solved https://www.hackerrank.com/challenges/birthday-cake-candles/problem

---
+++ CSharp/com.hackerrank/ColleenBirthdayCakeCandles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    /// <summary>
+    /// Colleen is having a birthday! She will have a cake with one candle for each year of her age. When she blows out the candles, she’ll only be able to blow out the tallest ones.
+    ///Find and print the number of candles she can successfully blow out.
+    /// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    /// </summary>
+    public class ColleenBirthdayCakeCandles
+    {
+        public int Solve(int age, int[] candleHeights)
+        {
+            if (age > 0 && candleHeights.Length > 0)
+            {
+                int tallestCandleHeight = candleHeights[0];
+                int noOfCandlesColleenCanBlow = 0;
+                for (int i = 0; i < candleHeights.Length; i++)
+                {
+                    if (candleHeights[i] > tallestCandleHeight)
+                    {
+                        noOfCandlesColleenCanBlow = 1;
+                        tallestCandleHeight = candleHeights[i];
+                    }
+                    else if (candleHeights[i] == tallestCandleHeight)
+                    {
+                        noOfCandlesColleenCanBlow++;
+                    }
+                }
+                return noOfCandlesColleenCanBlow;
+            }
+            return -1;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/ColleenBirthdayCakeCandlesTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class ColleenBirthdayCakeCandlesTests
+    {
+        [TestMethod()]
+        public void ColleenBirthdayCakeCandlesSolveTestCase1()
+        {
+            var instance = new ColleenBirthdayCakeCandles();
+            var expectedOutput = 2;
+            var age = 4;
+            var candleHeights = new int[] { 3, 2, 1, 3 };
+            var actualOutput = instance.Solve(age, candleHeights);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ColleenBirthdayCakeCandlesSolveTestCase2()
+        {
+            var instance = new ColleenBirthdayCakeCandles();
+            var expectedOutput = 4;
+            var age = 4;
+            var candleHeights = new int[] { 3, 3, 3, 3 };
+            var actualOutput = instance.Solve(age, candleHeights);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ColleenBirthdayCakeCandlesSolveTestCase3()
+        {
+            var instance = new ColleenBirthdayCakeCandles();
+            var expectedOutput = 1;
+            var age = 4;
+            var candleHeights = new int[] { 2, 2, 1, 3 };
+            var actualOutput = instance.Solve(age, candleHeights);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ColleenBirthdayCakeCandlesSolveTestCase4()
+        {
+            var instance = new ColleenBirthdayCakeCandles();
+            var expectedOutput = 1;
+            var age = 4;
+            var candleHeights = new int[] { 3, 2, 1, 1 };
+            var actualOutput = instance.Solve(age, candleHeights);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ColleenBirthdayCakeCandlesSolveTestCase5()
+        {
+            var instance = new ColleenBirthdayCakeCandles();
+            var expectedOutput = 1;
+            var age = 1;
+            var candleHeights = new int[] { 3 };
+            var actualOutput = instance.Solve(age, candleHeights);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ColleenBirthdayCakeCandlesSolveTestCase6()
+        {
+            var instance = new ColleenBirthdayCakeCandles();
+            var expectedOutput = -1;
+            var age = 0;
+            var candleHeights = new int[] { 3 };
+            var actualOutput = instance.Solve(age, candleHeights);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ColleenBirthdayCakeCandlesSolveTestCase7()
+        {
+            var instance = new ColleenBirthdayCakeCandles();
+            var expectedOutput = -1;
+            var age = 1;
+            var candleHeights = new int[] { };
+            var actualOutput = instance.Solve(age, candleHeights);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
