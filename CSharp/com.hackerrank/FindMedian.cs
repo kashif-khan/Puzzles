ff99b03c52ca6a771ca6f4b67b5015a48fb53f1c
kashif-khan <uplink2010@gmail.com>
2018-03-25 07:18:10 +0530

    Find median still unresolved

---
+++ CSharp/com.hackerrank/FindMedian.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class FindMedian
+    {
+        public int Solve(int[] input)
+        {
+            if (input.Length > 0)
+            {
+                int sum = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    sum += input[i];
+                }
+                return sum / input.Length;
+            }
+            return -1;
+        }
+    }
+}
