83b341748c64b771e775b0da22d2bffe3f967214
Kashif Khan <kashif.k.khan@ericsson.com>
2018-12-17 16:11:46 +0530

    Added case sensitive palindrome tests and its respective algorithn

---
+++ CSharp/Algorithm/Palindrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Algorithm
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome(string input, bool IsCaseSensitive = true)
+        {
+            if (IsCaseSensitive)
+            {
+                input = input.ToLowerInvariant();
+            }
+            int head = 0, tail = input.Length - 1;
+            while (head <= tail && !input[head--].Equals(input[tail--]))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ CSharpTests/Algorithm/PalindromeTests.cs
@@ -0,0 +1,62 @@
+﻿using CSharp.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Algorithm.Tests
+{
+    [TestClass()]
+    public class PalindromeTests
+    {
+        [TestMethod()]
+        public void TrueCaseSensitivePalindromeEvenLengthString()
+        {
+            var input = "hannah";
+            var expectedOutput = true;
+            var sut = new Palindrome();
+
+            var actualOutput = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void TrueCaseSensitivePalindromeOddLengthString()
+        {
+            var input = "civic";
+            var expectedOutput = true;
+            var sut = new Palindrome();
+
+            var actualOutput = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void FalseCaseSensitivePalindromeEvenLengthString()
+        {
+            var input = "abcd";
+            var expectedOutput = false;
+            var sut = new Palindrome();
+
+            var actualOutput = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void FalseCaseSensitivePalindromeOddLengthString()
+        {
+            var input = "abcde";
+            var expectedOutput = false;
+            var sut = new Palindrome();
+
+            var actualOutput = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
