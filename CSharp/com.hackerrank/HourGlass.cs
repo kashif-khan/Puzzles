f2405184581f32b5a52ce96f1a0de1e9c8e73ccb
Kashif Khan <uplink2010@gmail.com>
2019-09-06 17:08:23 +0530

    Fixed the test case and the code of the HourGlass

--- CSharp/com.hackerrank/HourGlass.cs
+++ CSharp/com.hackerrank/HourGlass.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+#if DEBUG
 using System.Diagnostics;
+#endif
 using System.Linq;
 
 namespace CSharp.com.hackerrank
@@ -8,31 +10,35 @@ namespace CSharp.com.hackerrank
     /// https://www.hackerrank.com/challenges/2d-array/problem
     public class HourGlass
     {
-        public static int Solve(int[,] inputArray)
+        public static int Solve(int[][] inputArray)
         {
             var hourGlassValues = new HashSet<int>();
             int temp;
-            for (int rowIndex = 0; rowIndex < inputArray.GetLength(0) - 1; rowIndex++)
+            for (int rowIndex = 0; rowIndex + 3 <= inputArray.GetLength(0); rowIndex++)
             {
-                for (int columnIndex = 0; columnIndex < inputArray.GetLength(1) - 1; columnIndex++)
+                for (int columnIndex = 0; columnIndex + 3 <= inputArray.GetLength(0); columnIndex++)
                 {
                     temp = 0;
-                    if (rowIndex + 2 >= inputArray.GetLength(0) || columnIndex + 2 >= inputArray.GetLength(1))
-                        continue;
+#if DEBUG
                     Debug.WriteLine(string.Empty);
 
-                    Debug.WriteLine($"{inputArray[rowIndex, columnIndex]} {inputArray[rowIndex, columnIndex + 1]} {inputArray[rowIndex, columnIndex + 2]}");
-                    temp += inputArray[rowIndex, columnIndex];
-                    temp += inputArray[rowIndex, columnIndex + 1];
-                    temp += inputArray[rowIndex, columnIndex + 2];
+                    Debug.WriteLine($"{inputArray[rowIndex][columnIndex]} {inputArray[rowIndex][columnIndex + 1]} {inputArray[rowIndex][columnIndex + 2]}");
+#endif
+                    temp += inputArray[rowIndex][columnIndex];
+                    temp += inputArray[rowIndex][columnIndex + 1];
+                    temp += inputArray[rowIndex][columnIndex + 2];
 
-                    Debug.WriteLine($" {inputArray[rowIndex + 1, columnIndex + 1]} ");
-                    temp += inputArray[rowIndex + 1, columnIndex + 1];
+#if DEBUG
+                    Debug.WriteLine($" {inputArray[rowIndex + 1][columnIndex + 1]} ");
+#endif
+                    temp += inputArray[rowIndex + 1][columnIndex + 1];
 
-                    Debug.WriteLine($"{inputArray[rowIndex + 2, columnIndex]} {inputArray[rowIndex + 2, columnIndex + 1]} {inputArray[rowIndex + 2, columnIndex + 2]}");
-                    temp += inputArray[rowIndex + 2, columnIndex];
-                    temp += inputArray[rowIndex + 2, columnIndex + 1];
-                    temp += inputArray[rowIndex + 2, columnIndex + 2];
+#if DEBUG
+                    Debug.WriteLine($"{inputArray[rowIndex + 2][columnIndex]} {inputArray[rowIndex + 2][columnIndex + 1]} {inputArray[rowIndex + 2][columnIndex + 2]}");
+#endif
+                    temp += inputArray[rowIndex + 2][columnIndex];
+                    temp += inputArray[rowIndex + 2][columnIndex + 1];
+                    temp += inputArray[rowIndex + 2][columnIndex + 2];
                     hourGlassValues.Add(temp);
                 }
             }
--- CSharpTests/com.hackerrank/HourGlassTests.cs
+++ CSharpTests/com.hackerrank/HourGlassTests.cs
@@ -17,15 +17,14 @@ namespace CSharp.com.hackerrank.Tests
         [TestMethod()]
         public void HourGlassSolveTestCase1()
         {
-            var inputArray = new int[,]
-            {
-                {1, 1, 1, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0, 0},
-                {0, 0, 2, 4, 4, 0},
-                {0, 0, 0, 2, 0, 0},
-                {0, 0, 1, 2, 4, 0}
-            };
+            var inputArray = new int[6][];
+
+            inputArray[0] = new int[] { 1, 1, 1, 0, 0, 0 };
+            inputArray[1] = new int[] { 0, 1, 0, 0, 0, 0 };
+            inputArray[2] = new int[] { 1, 1, 1, 0, 0, 0 };
+            inputArray[3] = new int[] { 0, 0, 2, 4, 4, 0 };
+            inputArray[4] = new int[] { 0, 0, 0, 2, 0, 0 };
+            inputArray[5] = new int[] { 0, 0, 1, 2, 4, 0 };
             var outputArray = 19;
             int UniqueValue = HourGlass.Solve(inputArray);
             Assert.AreEqual(outputArray, UniqueValue);
