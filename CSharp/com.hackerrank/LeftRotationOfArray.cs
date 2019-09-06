69306391900f3adec60a42ba62af25e9522742ad
Kashif Khan <uplink2010@gmail.com>
2019-09-06 17:39:47 +0530

    Arrays: Left Rotation Code

---
+++ CSharp/com.hackerrank/LeftRotationOfArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    /// <summary>
+    /// Arrays - Left Rotation.pdf
+    /// </summary>
+    public class LeftRotationOfArray
+    {
+        public static int[] Solve(int[] inputArray, int numbersToShift)
+        {
+            var cursor = numbersToShift;
+            var rotatedArray = new int[inputArray.Length];
+            for (int iterator = 0; iterator < inputArray.Length; iterator++, cursor = ++cursor % inputArray.Length)
+            {
+                rotatedArray[iterator] = inputArray[cursor];
+            }
+            return rotatedArray;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/LeftRotationOfArrayTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class LeftRotationOfArrayTests
+    {
+        [TestMethod()]
+        public void LeftRotationOfArrayTestCase1()
+        {
+            int numbersToShift = 4;
+
+            int[] inputArray = Array.ConvertAll("1 2 3 4 5".Split(' '), aTemp => Convert.ToInt32(aTemp));
+            int[] expectedOutput = Array.ConvertAll("5 1 2 3 4".Split(' '), aTemp => Convert.ToInt32(aTemp));
+
+            int[] actualOutput = LeftRotationOfArray.Solve(inputArray, numbersToShift);
+
+            Assert.AreEqual(String.Join(" ", expectedOutput), string.Join(" ", actualOutput));
+        }
+
+
+    }
+}
