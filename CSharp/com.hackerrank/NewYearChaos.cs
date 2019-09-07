8903b6bdea27e0127a645f2df417fc6145fffcdb
Kashif Khan <uplink2010@gmail.com>
2019-09-07 23:22:07 +0530

    New Year Chaos

---
+++ CSharp/com.hackerrank/NewYearChaos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharp.com.hackerrank
+{
+    public class NewYearChaos
+    {
+        private const string ImpossibleMessage = "Too chaotic";
+        public static string Solve(int[] inputArray)
+        {
+            {
+                int minimumBribeCount = 0;
+                for (int iterator = inputArray.Length - 1; iterator >= 0; iterator--)
+                {
+                    if (inputArray[iterator] - (iterator + 1) > 2)
+                    {
+                        Console.WriteLine(ImpossibleMessage);
+                        return ImpossibleMessage;
+                    }
+                    for (int minimumPossiblePosition = Math.Max(0, inputArray[iterator] - 2); minimumPossiblePosition < iterator; minimumPossiblePosition++)
+                        if (inputArray[minimumPossiblePosition] > inputArray[iterator]) minimumBribeCount++;
+                }
+                Console.WriteLine(minimumBribeCount);
+                return minimumBribeCount.ToString();
+            }
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/NewYearChaosTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class NewYearChaosTests
+    {
+        private const string ImpossibleMessage = "Too chaotic";
+        [TestMethod()]
+        public void NewYearChaosTestCase1()
+        {
+            int[] input = Array.ConvertAll("2 1 5 3 4".Split(' '), qTemp => Convert.ToInt32(qTemp));
+            var expectedOutput = "3";
+
+            var actualOutput = NewYearChaos.Solve(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void NewYearChaosTestCase2()
+        {
+            int[] input = Array.ConvertAll("2 5 1 3 4".Split(' '), qTemp => Convert.ToInt32(qTemp));
+            var expectedOutput = ImpossibleMessage;
+
+            var actualOutput = NewYearChaos.Solve(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void NewYearChaosTestCase3()
+        {
+            int[] input = Array.ConvertAll("1 2 5 3 7 8 6 4".Split(' '), qTemp => Convert.ToInt32(qTemp));
+            var expectedOutput = "7";
+
+            var actualOutput = NewYearChaos.Solve(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
