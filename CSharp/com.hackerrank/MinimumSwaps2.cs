189d456b88eaaff049e48d5ad8da3909e6413ae0
Kashif Khan <uplink2010@gmail.com>
2019-09-08 12:46:16 +0530

    Minimum swaps 2

---
+++ CSharp/com.hackerrank/MinimumSwaps2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class MinimumSwaps2
+    {
+        public static int Solve(int[] inputArray)
+        {
+            int farthestCursor = inputArray.Length - 1;
+
+            int cursor = 0;
+            int minimumSwaps = 0;
+
+            while (cursor < inputArray.Length)
+            {
+                int trueCurrentValue = cursor + 1;
+
+                if (inputArray[cursor] == trueCurrentValue)
+                {
+                    cursor++;
+                    continue;
+                }
+
+                while (inputArray[farthestCursor] != trueCurrentValue)
+                {
+                    farthestCursor--;
+                }
+
+                if (farthestCursor != cursor)
+                {
+                    Swap(inputArray, cursor, farthestCursor);
+                    minimumSwaps++;
+                }
+
+                farthestCursor = inputArray.Length - 1;
+                cursor++;
+            }
+            return minimumSwaps;
+        }
+
+        private static void Swap(int[] inputArray, int iterator, int nextiterator)
+        {
+            inputArray[iterator] = inputArray[iterator] + inputArray[nextiterator];
+            inputArray[nextiterator] = inputArray[iterator] - inputArray[nextiterator];
+            inputArray[iterator] = inputArray[iterator] - inputArray[nextiterator];
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/MinimumSwaps2Tests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class MinimumSwaps2Tests
+    {
+        [TestMethod()]
+        public void MinimumSwaps2TestCase1()
+        {
+            int[] inputArray = Array.ConvertAll("2 3 4 1 5".Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int expectedOutput = 3;
+
+            int actualOutput = MinimumSwaps2.Solve(inputArray);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void MinimumSwaps2TestCase2()
+        {
+            int[] inputArray = Array.ConvertAll("1 3 5 2 4 6 7".Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int expectedOutput = 3;
+
+            int actualOutput = MinimumSwaps2.Solve(inputArray);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void MinimumSwaps2TestCase3()
+        {
+            int[] inputArray = Array.ConvertAll("4 3 1 2".Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int expectedOutput = 3;
+
+            int actualOutput = MinimumSwaps2.Solve(inputArray);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
