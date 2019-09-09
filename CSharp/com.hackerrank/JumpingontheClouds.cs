0dcf1d238a8dd3447b36e911222c0451590d8779
Kashif Khan <uplink2010@gmail.com>
2019-09-09 16:41:26 +0530

    Jumping On The Clouds

---
+++ CSharp/com.hackerrank/JumpingontheClouds.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class JumpingOnTheClouds
+    {
+        public static int Solve(int[] inputArray)
+        {
+            var minimumJumps = 0;
+            var jump = 0;
+            while (jump < inputArray.Length - 1)
+            {
+                if (jump + 2 < inputArray.Length && inputArray[jump + 2] != 1)
+                {
+                    jump += 2;
+                }
+                else
+                {
+                    jump++;
+                }
+                minimumJumps++;
+            }
+            return minimumJumps;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/JumpingOntheCloudsTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class JumpingOnTheCloudsTests
+    {
+        [TestMethod()]
+        public void JumpingOnTheCloudsTestCase1()
+        {
+            var clouds = new int[] { 0, 0, 1, 0, 0, 1, 0 };
+            var expectedJumps = 4;
+
+            var actualJumps = JumpingOnTheClouds.Solve(clouds);
+
+            Assert.AreEqual(expectedJumps, actualJumps);
+        }
+
+        [TestMethod()]
+        public void JumpingOnTheCloudsTestCase2()
+        {
+            var clouds = new int[] { 0, 0, 0, 0, 1, 0 };
+            var expectedJumps = 3;
+
+            var actualJumps = JumpingOnTheClouds.Solve(clouds);
+
+            Assert.AreEqual(expectedJumps, actualJumps);
+        }
+
+        [TestMethod()]
+        public void JumpingOnTheCloudsTestCase3()
+        {
+            var clouds = new int[] { 0, 0, 0, 1, 0, 0 };
+            var expectedJumps = 3;
+
+            var actualJumps = JumpingOnTheClouds.Solve(clouds);
+
+            Assert.AreEqual(expectedJumps, actualJumps);
+        }
+    }
+}
