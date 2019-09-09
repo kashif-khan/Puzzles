706a848cfe9414d3582e5e3637c790e42c403fa0
Kashif Khan <uplink2010@gmail.com>
2019-09-09 17:14:31 +0530

    Counting Valleys

---
+++ CSharp/com.hackerrank/CountingValleys.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class CountingValleys
+    {
+        public static int Solve(int stepCount, string steps)
+        {
+            var valleyCount = 0;
+            int altimeter = 0;
+            var inValley = false;
+            for (int eachStepIndex = 0; eachStepIndex < steps.Length; eachStepIndex++)
+            {
+                switch (steps[eachStepIndex])
+                {
+                    case 'U':
+                        altimeter++;
+                        break;
+                    case 'D':
+                        altimeter--;
+                        break;
+                }
+
+                if (altimeter < 0)
+                {
+                    inValley = true;
+                }
+
+                if (altimeter == 0)
+                {
+                    if (inValley)
+                    {
+                        valleyCount++;
+                    }
+                    inValley = false;
+                }
+            }
+            return valleyCount;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/CountingValleysTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class CountingValleysTests
+    {
+        [TestMethod()]
+        public void CountingValleysTestCase1()
+        {
+            int stepCount = 8;
+            string steps = "UDDDUDUU";
+            int expectedOutput = 1;
+
+            int actualOutput = CountingValleys.Solve(stepCount, steps);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
