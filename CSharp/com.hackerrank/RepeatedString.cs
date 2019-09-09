a8039850748981997b9943c0158cf0fd7b2559c8
Kashif Khan <uplink2010@gmail.com>
2019-09-09 16:14:10 +0530

    Repeated String

---
+++ CSharp/com.hackerrank/RepeatedString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class RepeatedString
+    {
+        private const char characterToCompare = 'a';
+
+        public static long Solve(string inputString, long repeatCount)
+        {
+            long count = 0;
+            long residueIndex = inputString.Length;
+            long multiplier = 0;
+            long residueCount = 0, multiplierCount = 0;
+
+            residueIndex = repeatCount % inputString.Length;
+            multiplier = repeatCount / inputString.Length;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == characterToCompare)
+                {
+                    if (i < residueIndex)
+                    {
+                        residueCount++;
+                    }
+                    multiplierCount++;
+                }
+
+            }
+
+            count = (multiplier * multiplierCount) + residueCount;
+            return count;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/RepeatedStringTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class RepeatedStringTests
+    {
+        [TestMethod()]
+        public void RepeatedStringTestCase1()
+        {
+            string inputString = "aba";
+
+            long repeatCount = 10;
+
+            long expectedOutput = 7;
+
+            long actualOutput = RepeatedString.Solve(inputString, repeatCount);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void RepeatedStringTestCase2()
+        {
+            string inputString = "a";
+
+            long repeatCount = 1000000000000;
+
+            long expectedOutput = 1000000000000;
+
+            long actualOutput = RepeatedString.Solve(inputString, repeatCount);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void RepeatedStringTestCase3()
+        {
+            string inputString = "abcac";
+
+            long repeatCount = 10;
+
+            long expectedOutput = 4;
+
+            long actualOutput = RepeatedString.Solve(inputString, repeatCount);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
