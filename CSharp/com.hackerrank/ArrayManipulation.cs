cee6cd3dcfd1d1aa6cd111145c1ec488d9f79925
Kashif Khan <uplink2010@gmail.com>
2019-09-09 15:26:14 +0530

    Array Manipulation

---
+++ CSharp/com.hackerrank/ArrayManipulation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class ArrayManipulation
+    {
+        public static long Solve(uint specimenArrayLength, int[][] queries)
+        {
+            long[] numList = new long[specimenArrayLength + 1];
+
+            for (int iterator = 0; iterator < queries.GetLength(0); iterator++)
+            {
+                uint fromIndex = (UInt32)queries[iterator][0];
+                uint toIndex = (UInt32)queries[iterator][1];
+                long valueToBeAdded = (long)queries[iterator][2];
+
+                numList[fromIndex] += valueToBeAdded;
+                if (toIndex + 1 <= specimenArrayLength) numList[toIndex + 1] -= valueToBeAdded;
+            }
+
+            long tempMax = 0;
+            long maximumNumber = 0;
+            for (int iterator = 1; iterator <= specimenArrayLength; iterator++)
+            {
+                tempMax += numList[iterator];
+                if (tempMax > maximumNumber) maximumNumber = tempMax;
+            }
+
+            return maximumNumber;
+        }
+
+        private static long MyOldMethod(uint specimenArrayLength, int[][] queries)
+        {
+            long maximumNumber = 0;
+
+            var specimenArray = new long[specimenArrayLength];
+            for (int queryIterator = 0; queryIterator < queries.GetLength(0); queryIterator++)
+            {
+                int fromIndex = queries[queryIterator][0] - 1;
+                int toIndex = queries[queryIterator][1] - 1;
+                int valueToBeAdded = queries[queryIterator][2];
+                for (int specimenIterator = fromIndex; specimenIterator <= toIndex; specimenIterator++)
+                {
+                    specimenArray[specimenIterator] += valueToBeAdded;
+                    if (maximumNumber < specimenArray[specimenIterator])
+                    {
+                        maximumNumber = specimenArray[specimenIterator];
+                    }
+                }
+            }
+
+            return maximumNumber;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/ArrayManipulationTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class ArrayManipulationTests
+    {
+        [TestMethod()]
+        public void ArrayManipulationTestCase1()
+        {
+            uint specimenArrayLength = 5;
+
+            int[][] queries = new int[3][];
+
+            queries[0] = Array.ConvertAll("1 2 100".Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+            queries[1] = Array.ConvertAll("2 5 100".Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+            queries[2] = Array.ConvertAll("3 4 100".Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+
+            var expectedOutput = 200;
+
+            long actualOutput = ArrayManipulation.Solve(specimenArrayLength, queries);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ArrayManipulationTestCase2()
+        {
+            uint specimenArrayLength = 10;
+
+            int[][] queries = new int[3][];
+
+            queries[0] = Array.ConvertAll("1 5 3".Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+            queries[1] = Array.ConvertAll("4 8 7".Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+            queries[2] = Array.ConvertAll("6 9 1".Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+
+            var expectedOutput = 10;
+
+            long actualOutput = ArrayManipulation.Solve(specimenArrayLength, queries);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
