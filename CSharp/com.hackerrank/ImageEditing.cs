72781841f90390b471dc64c9ae56ea8ccc103238
Kashif Khan <uplink2010@gmail.com>
2019-09-26 21:15:11 +0530

    ImageEditing

---
+++ CSharp/com.hackerrank/ImageEditing.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class ImageEditing
+    {
+        public static int Solve(List<List<int>> arr)
+        {
+            int[,] inputArray = ConvertToMultidimensionalArray(arr);
+
+            int rowIndex, columnIndex;
+
+            int rows = inputArray.GetLength(0);
+
+            int columns = inputArray.GetLength(1);
+
+            int[,] submatrix = new int[rows, columns];
+
+            int submatrixMax, rowMax, columnMax;
+
+            InitializeSubmatrix(inputArray, out rowIndex, out columnIndex, rows, columns, submatrix);
+
+            rowIndex = CalculateSubmatrix(inputArray, ref columnIndex, rows, columns, submatrix);
+
+            submatrixMax = submatrix[0, 0];
+            rowMax = 0;
+            columnMax = 0;
+
+            rowIndex = FindMaxEntry(ref columnIndex, rows, columns, submatrix, ref submatrixMax, ref rowMax, ref columnMax);
+
+            return submatrixMax;
+        }
+
+        private static int FindMaxEntry(ref int columnIndex, int rows, int columns, int[,] submatrix, ref int submatrixMax, ref int rowMax, ref int columnMax)
+        {
+            int rowIndex;
+            /* Find the maximum entry, and indexes of  maximum entry in submatrixMax[, ] */
+            for (rowIndex = 0; rowIndex < rows; rowIndex++)
+            {
+                for (columnIndex = 0; columnIndex < columns; columnIndex++)
+                {
+                    if (submatrixMax < submatrix[rowIndex, columnIndex])
+                    {
+                        submatrixMax = submatrix[rowIndex, columnIndex];
+                        rowMax = rowIndex;
+                        columnMax = columnIndex;
+                    }
+                }
+            }
+
+            return rowIndex;
+        }
+
+        private static int CalculateSubmatrix(int[,] inputArray, ref int columnIndex, int rows, int columns, int[,] submatrix)
+        {
+            int rowIndex;
+
+            /* Construct other entries of submatrix[, ]*/
+            for (rowIndex = 1; rowIndex < rows; rowIndex++)
+            {
+                for (columnIndex = 1; columnIndex < columns; columnIndex++)
+                {
+                    if (inputArray[rowIndex, columnIndex] == 1)
+                        submatrix[rowIndex, columnIndex] = Math.Min(submatrix[rowIndex, columnIndex - 1], Math.Min(submatrix[rowIndex - 1, columnIndex], submatrix[rowIndex - 1, columnIndex - 1])) + 1;
+                    else
+                        submatrix[rowIndex, columnIndex] = 0;
+                }
+            }
+
+            return rowIndex;
+        }
+
+        private static void InitializeSubmatrix(int[,] inputArray, out int rowIndex, out int columnIndex, int rows, int columns, int[,] submatrix)
+        {
+            /* Set first column of submatrix[, ]*/
+            for (rowIndex = 0; rowIndex < rows; rowIndex++)
+                submatrix[rowIndex, 0] = inputArray[rowIndex, 0];
+
+            /* Set first row of submatrix[, ]*/
+            for (columnIndex = 0; columnIndex < columns; columnIndex++)
+                submatrix[0, columnIndex] = inputArray[0, columnIndex];
+        }
+
+        private static int[,] ConvertToMultidimensionalArray(List<List<int>> arr)
+        {
+            var M = new int[arr.Count, arr.Count];
+
+            int rowIndex = 0, columnIndex = 0;
+            foreach (var row in arr)
+            {
+                columnIndex = 0;
+                foreach (var rowColumnValue in row)
+                {
+                    M[rowIndex, columnIndex++] = rowColumnValue;
+                }
+                rowIndex++;
+            }
+
+            return M;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/ImageEditingTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class ImageEditingTests
+    {
+        [TestMethod()]
+        public void ImageEditingTestCase1()
+        {
+            var dimension = 3;
+            var image = new string[]
+            {
+                "1 1 1",
+                "1 1 0",
+                "1 0 1"
+            };
+            var expectedOutput = 2;
+            var inputData = PrepareData(dimension, dimension, image);
+
+            var actualOutput = ImageEditing.Solve(inputData);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void ImageEditingTestCase2()
+        {
+            var dimension = 5;
+            var image = new string[]
+            {
+                "1 1 1 1 1",
+                "1 1 1 0 0",
+                "1 1 1 0 0",
+                "1 1 1 0 0",
+                "1 1 1 1 1"
+            };
+            var expectedOutput = 3;
+            var inputData = PrepareData(dimension, dimension, image);
+
+            var actualOutput = ImageEditing.Solve(inputData);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        private List<List<int>> PrepareData(int arrRows, int arrColumns, params string[] values)
+        {
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < arrRows; i++)
+            {
+                arr.Add(values[i].TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+            return arr;
+        }
+    }
+}
