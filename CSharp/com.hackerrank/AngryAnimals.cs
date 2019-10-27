cf261bfe20ccaeea3be9cefbfccaef053331ea91
Kashif Khan <uplink2010@gmail.com>
2019-10-28 03:49:24 +0530

    Angry Animals

---
+++ CSharp/com.hackerrank/AngryAnimals.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+#if DEBUG
+using System.Diagnostics;
+#endif
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class AngryAnimals
+    {
+        public static long Solve(int animalsCount, List<int> herbivorusAnimals, List<int> carnivorousAnimals)
+        {
+            int possibleCompatibleAnimalsCabinCount = 0;
+            var incompatibleAnimals = new Dictionary<int, int>();
+            var min = 0;
+
+            for (int incompatibleAnimalsIndex = 0; incompatibleAnimalsIndex < herbivorusAnimals.Count; incompatibleAnimalsIndex++)
+            {
+                if (herbivorusAnimals[incompatibleAnimalsIndex] == carnivorousAnimals[incompatibleAnimalsIndex])
+                    continue;
+
+                min = Math.Min(herbivorusAnimals[incompatibleAnimalsIndex], carnivorousAnimals[incompatibleAnimalsIndex]);
+                var max = Math.Max(herbivorusAnimals[incompatibleAnimalsIndex], carnivorousAnimals[incompatibleAnimalsIndex]);
+
+                if (!incompatibleAnimals.TryGetValue(min, out var current))
+                    incompatibleAnimals.Add(min, max);
+
+                if (current > max)
+                    incompatibleAnimals[min] = max;
+            }
+
+            var earlyEdge = animalsCount + 1;
+            for (int edgeIndex = animalsCount; edgeIndex >= 1; edgeIndex--)
+            {
+                var add = earlyEdge;
+                if (incompatibleAnimals.TryGetValue(edgeIndex, out var enemy))
+                {
+                    if (earlyEdge > enemy)
+                    {
+                        earlyEdge = enemy;
+                        add = enemy;
+                    }
+                }
+
+                possibleCompatibleAnimalsCabinCount += (add - edgeIndex);
+            }
+
+            return possibleCompatibleAnimalsCabinCount;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/AngryAnimalsTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class AngryAnimalsTests
+    {
+        [TestMethod()]
+        public void AngryAnimalsTestCase1()
+        {
+            int animalsCount = 4;
+            var herbivorus = Array.ConvertAll("1 2".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("3 4".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 7;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase2()
+        {
+            int animalsCount = 5;
+            var herbivorus = Array.ConvertAll("1 2".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("3 5".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 11;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase3()
+        {
+            int animalsCount = 8;
+            var herbivorus = Array.ConvertAll("2 3 4 3".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("8 5 6 4".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 18;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase4()
+        {
+            int animalsCount = 4;
+            var herbivorus = Array.ConvertAll("3 4".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("1 2".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 7;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase5()
+        {
+            int animalsCount = 5;
+            var herbivorus = Array.ConvertAll("3 5".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("1 2".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 11;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase6()
+        {
+            int animalsCount = 8;
+            var herbivorus = Array.ConvertAll("8 5 6 4".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("2 3 4 3".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 18;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase7()
+        {
+            int animalsCount = 8;
+            var herbivorus = Array.ConvertAll("8 5 4 4".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("2 3 6 3".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 18;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase8()
+        {
+            int animalsCount = 8;
+            var herbivorus = Array.ConvertAll("5 4 4 8".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("3 6 3 2".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 18;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod()]
+        public void AngryAnimalsTestCase9()
+        {
+            int animalsCount = 8;
+            var herbivorus = Array.ConvertAll("5 4 4 8 1".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var carnivorous = Array.ConvertAll("3 6 3 2 8".Split(' '), aTemp => Convert.ToInt32(aTemp)).ToList();
+            var expectedOutput = 18;
+
+
+            var actualOutput = AngryAnimals.Solve(animalsCount, herbivorus, carnivorous);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
