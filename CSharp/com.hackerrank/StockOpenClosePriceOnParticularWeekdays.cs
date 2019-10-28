a648fc695b34af43f37c9e6fea037710ffa2ad43
Kashif Khan <uplink2010@gmail.com>
2019-10-28 15:10:33 +0530

    Stock Open and Close Price on Particualr Weekdays

---
+++ CSharp/com.hackerrank/StockOpenClosePriceOnParticularWeekdays.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace CSharp.com.hackerrank
+{
+    public class StockOpenClosePriceOnParticularWeekdays
+    {
+        public static string[] Solve(string firstDate, string lastDate, string weekDay)
+        {
+            var fromDate = DateTime.Parse(firstDate);
+            var toDate = DateTime.Parse(lastDate);
+
+            var finalResult = new List<string>();
+
+            var startDate = fromDate;
+            for (int i = 0; i < 7; i++)
+            {
+                if (startDate.DayOfWeek.ToString() != weekDay)
+                {
+                    startDate = startDate.AddDays(1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (DateTime currentDate = startDate; currentDate < toDate; currentDate = currentDate.AddDays(7))
+            {
+                var url = $"https://jsonmock.hackerrank.com/api/stocks/search?date={currentDate.ToString("d-MMMM-yyyy")}";
+                var json = new WebClient().DownloadString(url);
+                var stockResults = JsonConvert.DeserializeObject<StockResults>(json);
+                if (stockResults.total_pages > 0)
+                {
+                    var dayPrice = stockResults.data.Where(i => i.date == currentDate).FirstOrDefault();
+                    if (dayPrice != null)
+                    {
+                        finalResult.Add($"{dayPrice.date.ToString("d-MMMM-yyyy")} {dayPrice.open} {dayPrice.close}");
+                        for (int pageNumber = 2; pageNumber <= stockResults.total_pages; pageNumber++)
+                        {
+                            url = $"https://jsonmock.hackerrank.com/api/stocks/search?date={currentDate.ToString("d-MMMM-yyyy")}&page={pageNumber}";
+                            json = new WebClient().DownloadString(url);
+                            stockResults = JsonConvert.DeserializeObject<StockResults>(json);
+                            dayPrice = stockResults.data.Where(i => i.date == currentDate).First();
+                            finalResult.Add($"{dayPrice.date.ToString("d-MMMM-yyyy")} {dayPrice.open} {dayPrice.close}");
+                        }
+                    }
+                }
+            }
+
+            return finalResult.ToArray(); ;
+        }
+    }
+
+    public class StockResults
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public StockData[] data { get; set; }
+        public class StockData
+        {
+            public DateTime date { get; set; }
+            public double open { get; set; }
+            public double close { get; set; }
+        }
+    }
+}
+
---
+++ CSharpTests/com.hackerrank/StockOpenClosePriceOnParticularWeekdaysTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class StockOpenClosePriceOnParticularWeekdaysTests
+    {
+        [TestMethod()]
+        public void StockOpenClosePriceOnParticularWeekdaysTestCase1()
+        {
+            var fromDate = "1-January-2000";
+            var toDate = "22-February-2000";
+            var weekDay = "Monday";
+            var expectedOutput = new string[]
+            {
+                "17-January-2000 5617.7 5404.07",
+                "31-January-2000 5338.67 5205.29",
+                "7-February-2000 5431.55 5474",
+                "14-February-2000 6130.23 5924.31",
+                "21-February-2000 5874.25 5876.89"
+            };
+
+            var actualOutput = StockOpenClosePriceOnParticularWeekdays.Solve(fromDate, toDate, weekDay);
+
+            Assert.IsTrue(actualOutput.Count() == expectedOutput.Count(), "Expected output count does not match with actual output count");
+
+            foreach (var item in expectedOutput)
+            {
+                Assert.IsTrue(actualOutput.Contains(item), $"{item} does not exist in the output");
+            }
+        }
+    }
+}
