1eee04226673b1d476573f3302a38a96a16f3148
Kashif Khan <uplink2010@gmail.com>
2021-02-20 23:56:52 +0530

    SalesByMatch Puzzle

---
+++ CSharp/com.hackerrank/SalesByMatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class SalesByMatch
+    {
+        /// <summary>
+        /// Returns the number of pairs exist in the <paramref name="ar"/>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="ar"></param>
+        /// <returns></returns>
+        public static int SockMerchant(int n, int[] ar)
+        {
+            var lookup = new Dictionary<int, int>();
+            foreach (var item in ar)
+            {
+                if (lookup.ContainsKey(item))
+                {
+                    lookup[item] += 1;
+                }
+                else
+                {
+                    lookup[item] = 1;
+                }
+            }
+
+            int pairsCount = 0;
+
+            foreach (var item in lookup)
+            {
+                pairsCount += (item.Value / 2);
+            }
+            return pairsCount;
+        }
+    }
+}
---
+++ CSharpTests/com.hackerrank/SalesByMatchTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.com.hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank.Tests
+{
+    [TestClass()]
+    public class SalesByMatchTests
+    {
+        [TestMethod()]
+        public void SockMerchantTest()
+        {
+            int[] ar = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            int n = ar.Length;
+
+            int result = SalesByMatch.SockMerchant(n, ar);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
