a646335bbbd547fb52a125145a09bbdbf3ac12c2
Kashif Khan <uplink2010@gmail.com>
2021-02-21 16:17:46 +0530

    Ransom Note

---
+++ CSharp/com.hackerrank/RansomNote.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.com.hackerrank
+{
+    public class RansomNote
+    {
+        // Complete the checkMagazine function below.
+        static void CheckMagazine(string[] magazine, string[] note)
+        {
+            const string Yes = nameof(Yes);
+            const string No = nameof(No);
+            if ((magazine.Length < note.Length) || magazine.Length < 1 || note.Length < 0)
+            {
+                Console.WriteLine(No);
+            }
+
+            var magWords = new Dictionary<string, int>();
+            var noteWords = new Dictionary<string, int>();
+
+            foreach (var word in magazine)
+            {
+                if (!magWords.ContainsKey(word))
+                {
+                    magWords.Add(word, 1);
+                }
+                else
+                {
+                    magWords[word] += 1;
+                }
+            }
+
+            foreach (var word in note)
+            {
+                if (!noteWords.ContainsKey(word))
+                {
+                    noteWords.Add(word, 1);
+                }
+                else
+                {
+                    noteWords[word] += 1;
+                }
+            }
+
+            foreach (var item in noteWords)
+            {
+                if (magWords.ContainsKey(item.Key) && magWords[item.Key] >= item.Value)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(No);
+                    return;
+                }
+            }
+
+            Console.WriteLine(Yes);
+        }
+    }
+}
---
+++ SandboxConsoleAppCore/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(string[] magazine, string[] note)
+    {
+        const string Yes = nameof(Yes);
+        const string No = nameof(No);
+
+        if ((magazine.Length < note.Length) || magazine.Length < 1 || note.Length < 0)
+        {
+            Console.WriteLine(No);
+            return;
+        }
+
+        var magWords = new Dictionary<string, int>();
+        var noteWords = new Dictionary<string, int>();
+
+        foreach (var word in magazine)
+        {
+            if (!magWords.ContainsKey(word))
+            {
+                magWords.Add(word, 1);
+            }
+            else
+            {
+                magWords[word] += 1;
+            }
+        }
+
+        foreach (var word in note)
+        {
+            if (!noteWords.ContainsKey(word))
+            {
+                noteWords.Add(word, 1);
+            }
+            else
+            {
+                noteWords[word] += 1;
+            }
+        }
+
+        foreach (var item in noteWords)
+        {
+            if (magWords.ContainsKey(item.Key) && magWords[item.Key] >= item.Value)
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine(No);
+                return;
+            }
+        }
+
+        Console.WriteLine(Yes);
+    }
+
+    static void Main(string[] args)
+    {
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        string[] magazine = Console.ReadLine().Split(' ');
+
+        string[] note = Console.ReadLine().Split(' ');
+
+        checkMagazine(magazine, note);
+    }
+}
